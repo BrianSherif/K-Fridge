bd2b0fd7a61b37ecf46c77b3de1eb2fbe54105ad
fmanana <fezile.manana@protonmail.com>
2019-11-16 14:23:29 +0100

    adding static pages

--- app.py
+++ app.py
@@ -1,2 +1,19 @@
-from flask import Flask
+from flask import Flask, request, render_template, url_for, send_from_directory
+import os
 
+static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'static')
+
+app = Flask(__name__, template_folder=static_file_dir, static_folder=static_file_dir)
+
+
+@app.route('/home')
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('/about-us/index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True, port='5000')
