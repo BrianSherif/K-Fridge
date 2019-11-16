bff57859c0eaf4a4fb5874e980aaa5edbdc7ddd9
fmanana <fezile.manana@protonmail.com>
2019-11-16 16:23:01 +0100

    add database

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from flask import Flask, request, render_template, url_for, send_from_directory
+from flask import Flask, request, render_template, url_for, send_from_directory, session
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from flask_socketio import SocketIO, send
 import os
 
 static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'static')
@@ -6,8 +10,20 @@ static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'sta
 app = Flask(__name__, template_folder=static_file_dir, static_folder=static_file_dir)
 
 
+engine = create_engine('sqlite:///db/kommunity.db', echo=True)
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/kommunity.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'AbReheSO2uksoMbZpagPHpuCdKY3R3DqiFIfLDy5K6I0XFOEOgMOfgwGB5pHeatI'
+db = SQLAlchemy(app)
+socketio = SocketIO(app)
+
+def generate_random(n=64):
+    return ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(n))
+
 @app.route('/home')
-@app.route('/', methods=['GET'])
+@app.route('/')
 def home():
     return render_template('index.html')
 
@@ -17,10 +33,52 @@ def about():
 
 @app.route('/login')
 def login():
-    return render_template('/login/index.html')
+    if session.get('userid'):
+        return redirect(url_for('recipes'))
+    else:
+        return render_template('/login/index.html')
 
-@app.route('/recipes')
-def recipes():
+# logout
+
+@app.route('/auth', methods=['GET', 'POST'])
+def auth():
+    post_username = str(request.form['username'])
+    post_password = str(request.form['password'])
+
+    random_token = generate_random()
+    # print(random_token)
+    session[random_token] = {
+        'username': post_username,
+        'password': post_password
+    }
+    
+    return redirect(url_for('login_auth', token=random_token))
+
+@app.route('/login-auth/<token>')
+def login_auth(token):
+    if not session[token]:
+        return redirect(url_for('recipes'))
+    post_username = session[token]['username']
+    post_password = session[token]['password']
+    session.pop(token, None)
+
+    Session = sessionmaker(bind=engine)
+    s = Session()
+
+    result = s.query(user).filter(user.username.in_([post_username]), user.password.in_([post_password])).first()
+    print(result)
+    if result:
+        query = s.query(user).filter(user.username.in_([post_username])).first()
+        ingredients = query.items
+        session['logged_in'] = True
+        session['userid'] = query.userid
+        return redirect(url_for('recipes', user=query))
+    else:
+        return redirect(url_for('login'))
+
+
+@app.route('/recipes', defaults={'user': None})
+def recipes(user):
     return render_template('/recipes/index.html')
 
 if __name__ == "__main__":
--- db/db.py
+++ db/db.py
@@ -6,13 +6,13 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+class user(db.Model):
+    userid = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    passwd = db.Column(db.String(120), unique=False, nullable=False)
+    password = db.Column(db.String(120), unique=False, nullable=False)
     items = db.Column(db.ARRAY(db.String(120)), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<user %r>' % self.username
 
