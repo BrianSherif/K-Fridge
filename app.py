c344c08e1ec1552540be6ae45239f8ae95e78ef8
fmanana <fezile.manana@protonmail.com>
2019-11-16 20:47:10 +0100

    fixed login

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
-from flask import Flask, request, render_template, url_for, send_from_directory, session
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from flask_socketio import SocketIO, send
+from flask import Flask, request, render_template, url_for, redirect, send_from_directory, session
+from flask_login import LoginManager
+import sqlite3
+import random
+import string
 import os
+from db.__init__ import *
 
 static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'static')
 
+is_logged_in = False
 app = Flask(__name__, template_folder=static_file_dir, static_folder=static_file_dir)
-
-
-engine = create_engine('sqlite:///db/kommunity.db', echo=True)
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/kommunity.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'AbReheSO2uksoMbZpagPHpuCdKY3R3DqiFIfLDy5K6I0XFOEOgMOfgwGB5pHeatI'
-db = SQLAlchemy(app)
-socketio = SocketIO(app)
+
+
+def connect_db():
+    try:
+        conn = sqlite3.connect('db/kommunity.db')
+    except Error as e:
+        print(e)
+
+    cursor = conn.cursor()
+    return cursor, conn
 
 def generate_random(n=64):
     return ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(n))
@@ -33,10 +36,10 @@ def about():
 
 @app.route('/login')
 def login():
-    if session.get('userid'):
-        return redirect(url_for('recipes'))
-    else:
+    if not session.get('logged_in'):
         return render_template('/login/index.html')
+    else:
+        return redirect(url_for('recipes'))
 
 # logout
 
@@ -44,42 +47,26 @@ def login():
 def auth():
     post_username = str(request.form['username'])
     post_password = str(request.form['password'])
-
-    random_token = generate_random()
-    # print(random_token)
-    session[random_token] = {
-        'username': post_username,
-        'password': post_password
-    }
     
-    return redirect(url_for('login_auth', token=random_token))
-
-@app.route('/login-auth/<token>')
-def login_auth(token):
-    if not session[token]:
-        return redirect(url_for('recipes'))
-    post_username = session[token]['username']
-    post_password = session[token]['password']
-    session.pop(token, None)
-
-    Session = sessionmaker(bind=engine)
-    s = Session()
-
-    result = s.query(user).filter(user.username.in_([post_username]), user.password.in_([post_password])).first()
+    cursor, conn = connect_db()
+    result = cursor.execute('SELECT * from user WHERE username="' + post_username + '"').fetchone()
+    
     print(result)
-    if result:
-        query = s.query(user).filter(user.username.in_([post_username])).first()
-        ingredients = query.items.split(',')
+    if check_encrypted_password(post_password, result[3]):
+        ingredients = result[4]
         session['logged_in'] = True
-        session['userid'] = query.userid
-        return redirect(url_for('recipes', user=query))
+        conn.close()
+        print(ingredients)
+        return recipes(ingredients)
     else:
+        conn.close()
         return redirect(url_for('login'))
 
 
-@app.route('/recipes', defaults={'user': None})
-def recipes(user):
-    return render_template('/recipes/index.html')
+@app.route('/recipes', defaults={'ingredients': None})
+def recipes(ingredients):
+    ingredients = ingredients.split(',')
+    return render_template('/recipes/index.html', ingredients=ingredients)
 
 if __name__ == "__main__":
     app.run(debug=True, port='5000')
